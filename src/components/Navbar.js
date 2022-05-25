e9d1e14c9235641489ee416718f9190b40c1b0c4
Pauline Schnoebelen <pauline.schnoebelen@gmail.com>
2022-05-25 17:18:33 +0200

    added hamburger menu in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,29 @@
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
-import "./Navbar.css";
+import "./Navbar.css"
+import icon from "../assets/icon.png"
 
 function Navbar(){
 
     const {isLoggedIn, isLoading, user, logOutUser} = useContext(AuthContext);
 
     return (
-        <nav className="Navbar">
-          <ul> 
-            <li> <NavLink to="/">Home</NavLink> | </li>
-            <li> <NavLink to="/meals">Today's Meals</NavLink> | </li>
-            <li> <NavLink to="/meals/create">Add New Meal</NavLink> ||| </li>
-            <li> <NavLink to="/mymeals">My Meals</NavLink> |||</li>
+        <section className="top-nav">
+        <div className="icon-container">
+        <img src={icon} className="icon" alt="icon"/>
+    </div>
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+        <div className="menu-button"></div>
+        </label>
+          <ul class ="menu"> 
+            <li> <NavLink to="/">Home</NavLink></li>
+            <li> <NavLink to="/meals">Today's Meals</NavLink></li>
+            <li> <NavLink to="/meals/create">Add New Meal</NavLink></li>
+            <li> <NavLink to="/mymeals">My Meals</NavLink></li>
             
-            <li>
+            <li className="left-side nav">
             { isLoggedIn &&
                 <>
                     <span>Hey there, {user.name} </span> 
@@ -27,13 +35,14 @@ function Navbar(){
 
             { !isLoggedIn &&
                 <>
-                    <NavLink to="/signup">Register</NavLink> | 
+                   {/*  <NavLink to="/signup">Register</NavLink> |  */}
                     <NavLink to="/login">Login</NavLink>
                 </>
             }
             </li>
           </ul> 
-        </nav>
+        
+        </section>
     );
 }
 
