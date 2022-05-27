856b2a985ce837a167d2c74c62581b2063422c58
Josipa314 <101945658+josipa314@users.noreply.github.com>
2022-05-27 10:21:01 +0200

    added images to about us page

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,5 +1,6 @@
 import "./AboutUsPage.css"
 import background from "../assets/top-view-lunchboxes-with-phone.jpg"
+import foodshare from "../assets/foodsharecollaborators.jpeg"
 import wework from "../assets/wework-logo.png"
 import qonto from "../assets/qonto-logo.png"
 import netflix from "../assets/netflix-logo2.jpg"
@@ -13,19 +14,19 @@ function AboutUs () {
       <main className="wrapper">
       <section className="container">
       <section className="about-container">
-        <div className="about-mission">
-        <br/>
-        <br/>
-          <p className="first-p">
+      <div className="about-mission">
+      <img src={foodshare} className="background-homepage" alt="background"/>
+
+          <p className="catch-phrase1">
             It's not always easy to meet coworkers in big companies. But fear
             not, ForkMate will help you break the ice. 
           </p>
-
           <p className="second-p">
             Did you bring too much food at work? You've organized a birthday
             party with your team and no one has touched the cake?
-            <br/> You're tired of eating alone at your desk? You forgot to pack
-            your lunch? You might not be the only one. 
+            <br/> You're tired of eating alone at your desk? 
+            <br/>You forgot to pack your lunch? 
+            <br/>You might not be the only one. 
             <br/> 
             <br/> 
             Login, browse the list of meals available in your building or add one yourself!
@@ -33,35 +34,25 @@ function AboutUs () {
           </p>  
         </div>
         <div className="our-mission-container">
-          <h2 className="about-title">Our mission</h2>
-          <p className="mission-statement2">
+          <h2>Our mission</h2>
+          <p className="catch-phrase2">
             Help people connect in the workplace and tackle food waste, all at once!
           </p>
 
-          <p className="mission-statement">
-            Help us spread the word by joining ForkMate! 
-            Become a forker and look for your coforkers.
-          </p>
-          <h3 className="catch-phrase2">Your forkmates are waiting for you.</h3>
         </div>
 
-        <article className="about-our-partners">
-          <div className="companies-partner">
+        <article className="our-partners-container">
           <h2 className="about-title">Our partners</h2>
-            <p className="mission-statement">
+          <p className="mission-statement2">
               Join the growing list of companies partering with us: Netflix
               (Los Angeles), IronHack (Madrid), Microsoft(San Francisco), ToGoodToGo (Paris) and more
-            </p>
-          </div>
+          </p>
           <div className="companies-icons">
-          <img src={ironhack} className="background-homepage" alt="background"/>
-          <img src={blablacar} className="background-homepage" alt="background"/>
-          <img src={qonto} className="background-homepage" alt="background"/>
-          <img src={netflix} className="background-homepage" alt="background"/>
-          <img src={wework} className="background-homepage" alt="background"/>
-            <p>
-              
-            </p>
+            <img src={netflix} className="background-homepage" alt="background"/>
+            <img src={qonto} className="background-homepage" alt="background"/>
+            <img src={wework} className="background-homepage" alt="background"/>
+            <img src={ironhack} className="background-homepage" alt="background"/>
+            <img src={blablacar} className="background-homepage" alt="background"/>
           </div>
         </article>
 
